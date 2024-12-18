c4ba5b1066d35cc01ac9545d25146d146e9db84d
MatiasOrtega <mat-1998-ort@hotmail.com>
2024-12-18 17:46:49 -0300

    Add suppressHydrationWarning to <html> tag in RootLayout component

--- frontend-coffee-shop/src/app/layout.tsx
+++ frontend-coffee-shop/src/app/layout.tsx
@@ -27,9 +27,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body
-        suppressHydrationWarning={true}
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider
